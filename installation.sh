66980a4022edb146ebc4c1e76ea373e0fedf6abb
Floris <florisrobart.pro@gmail.com>
2023-07-30 01:56:03 +0200

    transmission à la vm pour les tests

--- installation.sh
+++ installation.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
 
-mkdir /home/$USER/.maven_lite
-cp -f ./maven_lite.sh /home/$USER/.maven_lite/maven_lite.sh
+function copie() {
+    cp -f $fichier $dossier || {
+        echo "Erreur lors de la copie du fichier 'maven_lite.sh'."
+        exit 1
+    }
+}
+
+function addCommand() {
+    {
+        echo >> $bashrc
+        echo "# Maven Lite" >> $bashrc
+        echo "alias mvnl='bash $dossier/$fichier'" >> $bashrc
+
+        source $bashrc
+    } && {
+        echo "Installation terminée."
+        echo "Pour utiliser maven lite, tapez 'mvnl --help' dans un terminal."
+        return 0
+    } || {
+        echo "Erreur lors de l'ajout de la commande 'mvnl'."
+        exit 1
+    }
+}
+
+
+#======#
+# Main #
+#======#
+fichier="maven_lite.sh"
+dossier="/home/$USER/.maven_lite"
+bashrc="/home/$USER/.bashrc"
+
+ls $dossier >/dev/null 2>&1 && {
+    copie
+} || {
+    mkdir $dossier && { copie; } || {
+        echo "Erreur lors de la création du dossier '$dossier'."
+        exit 1
+    }
+}
+
+addCommand
