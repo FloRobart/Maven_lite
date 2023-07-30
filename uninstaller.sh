1244976394a176e53ffafef282c0fb055dce8bae
Floris <florisrobart.pro@gmail.com>
2023-07-30 02:38:15 +0200

    ajout de texte affiché à l'utilisateur

--- uninstaller.sh
+++ uninstaller.sh
@@ -7,6 +7,12 @@ fichier="maven_lite.sh"
 dossier="/home/$USER/.maven_lite"
 bashrc="/home/$USER/.bashrc"
 
-rm -fr $dossier
-
-sed -i '/# Maven Lite/,/# Fin Maven Lite/d' $bashrc
+rm -fr $dossier && {
+    sed -i '/# Maven Lite/,/# Fin Maven Lite/d' $bashrc && {
+        source $bashrc; echo "Maven Lite à bien été supprimé."
+    } || {
+        echo "Erreur lors la suppression de l'alias de Maven Lite dans le fichier '$bashrc'."
+    }
+} || {
+    echo "Erreur lors de la suppression du dossier '$dossier'."
+}
