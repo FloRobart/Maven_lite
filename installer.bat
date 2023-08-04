4d9269ca981d14a33799c37dd0e31f30f0a22e49
Floris <florisrobart.pro@gmail.com>
2023-08-04 17:11:59 +0200

    correction de bug

--- installer.bat
+++ installer.bat
@@ -1,14 +1,15 @@
 @echo off
 
 set "pathToInstallFolder=C:\Program Files\Maven_Lite"
-set "nomFichier=maven_lite.bat"
+set "nomFichier=mvnl.bat"
 
 if not exist "%pathToInstallFolder%" (
     mkdir "%pathToInstallFolder%" || ( echo Erreur lors de la création du dossier '%pathToInstallFolder%'. & exit /b 1 )
 )
 
-xcopy /s /y /q "%nomFichier%" "%pathToInstallFolder%" || ( echo Erreur lors de la copie du fichier '%nomFichier%' dans le dossier '%pathToInstallFolder%'. & exit /b 1 )
-
-setx path "%PATH%;%pathToInstallFolder%\%nomFichier%" || ( echo Erreur lors de l'ajout du dossier '%pathToInstallFolder%' dans la variable d'environnement 'PATH'. & exit /b 1 )
+copy /s /y /q "%nomFichier%" "%pathToInstallFolder%\%nomFichier%" || ( echo Erreur lors de la copie du fichier '%nomFichier%' dans le dossier '%pathToInstallFolder%'. & exit /b 1 )
 
 echo Installation terminée avec succès.
+exit /b 0
+setx path "%PATH%;%pathToInstallFolder%" || ( echo Erreur lors de l'ajout du dossier '%pathToInstallFolder%' dans la variable d'environnement 'PATH'. & exit /b 1 )
+
