201429b4ace5b4699c838e47af1f75e1d93144e5
Floris <florisrobart.pro@gmail.com>
2023-08-04 19:07:15 +0200

    désinstalleur fonctionnel

--- uninstaller.bat
+++ uninstaller.bat
@@ -1,16 +1,29 @@
 @echo off
 
+SETLOCAL ENABLEDELAYEDEXPANSION
 set "pathToInstallFolder=C:\Program Files\Maven_Lite"
-set "nomFichierSource=maven_lite.bat"
-set "nomFichierDestination=mvnl.bat"
+set "secondePath="
 
 if exist "%pathToInstallFolder%" (
     rmdir /Q /S "%pathToInstallFolder%" || ( echo Erreur lors de la suppression du dossier '%pathToInstallFolder%'. & exit /b 1 )
 )
 
-for /f "delims=" %%i in ('echo %PATH% ^| findstr /c:"%pathToInstallFolder%"') do (
-    echo i = '%%i'
+for %%i in ("%PATH:;=" "%") do (
+    set "var=%%i"
+    set "var=!var:"=!"
+
+    if not "!var!" == "%pathToInstallFolder%" (
+        if "!secondePath!" == "" (
+            set "secondePath=!var!"
+        ) else (
+            set "secondePath=!secondePath!;!var!"
+        )
+    )
 )
 
+setx path "%secondePath%" >nul || ( echo Erreur lors de la mdification de la variable d'environnement 'PATH' & exit /b 1 )
+
 echo Désinstallation terminée avec succès.
+
+ENDLOCAL
 exit /b 0
