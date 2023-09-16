0de0fc0ffb1092dc3cd49503c62589e04c44c2dc
Floris <florisrobart.pro@gmail.com>
2023-09-16 10:58:51 +0200

    ajout des pages de manuel à l'installeur

--- installer.sh
+++ installer.sh
@@ -3,6 +3,11 @@
 fichierOrig="maven_lite.sh"
 fichierDest="/usr/bin/mvnl"
 
+manpageLangages=("fr" "en")
+
+
+echo 'Installation de maven lite...'
+
 chmod +x $fichierOrig || {
     echo "Erreur lors du changement des droits du fichier 'maven_lite.sh'."
     exit 1
@@ -13,4 +18,23 @@ sudo cp -f $fichierOrig $fichierDest || {
     exit 1
 }
 
-echo 'Installation terminée avec succès.'
+echo 'Installation de maven lite terminée avec succès.'
+echo 'Ajout des pages de manuel dans les différentes langues...'
+
+for langage in "${manpageLangages[@]}"
+do
+    # pas sur que ça marche
+    manpageOrig="manpages/${langage}/mvnl.1"
+    manpageDest="/usr/local/man/${langage}/man1/"
+
+    echo "manpageOrig=\'$manpageOrig\'"
+    echo "manpageDest=\'$manpageDest\'"
+
+    sudo cp -f $manpageOrig $manpageDest || {
+        echo "Erreur lors de la copie du fichier '$manpageOrig'."
+        exit 1
+    }
+done
+
+echo 'Pages de manuel ajoutées avec succès.'
+exit 0
