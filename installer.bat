d35ef65d33973d1b9aa5931081c66dd6bbab1516
Floris <Floris.robart11@gmail.com>
2023-10-09 10:58:33 +0200

    correction de bug dans l'installeur windows

--- installer.bat
+++ installer.bat
@@ -1,19 +1,21 @@
 @echo off
 
-set "pathToInstallFolder=C:\Program Files\Maven_Lite"
-set "nomFichierSource=maven_lite.bat"
-set "nomFichierDestination=mvnl.bat"
+setlocal EnableDelayedExpansion
+    set "pathToInstallFolder=C:\Program Files\Maven_Lite"
+    set "nomFichierSource=maven_lite.bat"
+    set "nomFichierDestination=mvnl.bat"
 
-xcopy /s /y /q ".\%nomFichierSource%" "%pathToInstallFolder%\" >nul || ( echo Erreur lors de la copie du fichier '%nomFichierSource%' dans le dossier '%pathToInstallFolder%'. & exit /b 1 )
+    xcopy /s /y /q ".\%nomFichierSource%" "%pathToInstallFolder%\" >nul || ( echo Erreur lors de la copie du fichier '%nomFichierSource%' dans le dossier '%pathToInstallFolder%'. & exit /b 1 )
 
-if exist "%pathToInstallFolder%\%nomFichierDestination%" del /q /f "%pathToInstallFolder%\%nomFichierDestination%"
+    if exist "%pathToInstallFolder%\%nomFichierDestination%" del /q /f "%pathToInstallFolder%\%nomFichierDestination%"
 
-rename "%pathToInstallFolder%\%nomFichierSource%" "%nomFichierDestination%" >nul || ( echo Erreur lors du renomage du fichier '%nomFichierSource%'. & exit /b 1 )
+    rename "%pathToInstallFolder%\%nomFichierSource%" "%nomFichierDestination%" >nul || ( echo Erreur lors du renomage du fichier '%nomFichierSource%'. & exit /b 1 )
 
-echo %PATH% | findstr /c:"%pathToInstallFolder%" >nul 2>nul || (
-    setx path "%PATH%;%pathToInstallFolder%" >nul || ( echo Erreur lors de l'ajout du dossier '%pathToInstallFolder%' dans la variable d'environnement 'PATH'. & exit /b 1 )
-)
+    echo %PATH% | findstr /c:"%pathToInstallFolder%" >nul 2>nul || (
+        setx path "%PATH%;%pathToInstallFolder%" >nul || ( echo Erreur lors de l'ajout du dossier '%pathToInstallFolder%' dans la variable d'environnement 'PATH'. & exit /b 1 )
+    )
 
-echo Installation terminée avec succès.
+    echo Installation terminée avec succès.
+endlocal
 
 exit /b 0
