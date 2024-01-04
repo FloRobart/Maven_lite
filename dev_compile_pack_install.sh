8a3c59eb9ec01573d15338a778940ceebe011848
Floris <florisrobart.pro@gmail.com>
2024-01-04 04:04:46 +0100

    modification d'un fichier de developpement

--- dev_compile_pack_install.sh
+++ dev_compile_pack_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+javac -d maven-lite_2.0-1_all/etc/maven-lite -encoding UTF-8 src/MavenLite.java && echo -e "\nCompilation terminée avec succès\n" || { echo -e "\nErreur lors de la compilation\n" ; exit 1; }
 dpkg-deb --build maven-lite_2.0-1_all && (echo -e "\nPackage construit avec succès\n" ; sudo dpkg -i maven-lite_2.0-1_all.deb && echo -e "\nPackage installé avec succès\n" || echo -e "\nErreur lors de l'installation du package\n") || echo -e "\nErreur lors de la construction du package\n"
