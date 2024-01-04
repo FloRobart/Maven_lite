baaa9b367acbdd83058ea7511cd7ca27fe4725bc
Floris <florisrobart.pro@gmail.com>
2024-01-04 17:51:41 +0100

    suppression des fichier de dev inutile

--- dev_compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-javac -d maven-lite_2.0-1_all/etc/maven-lite -encoding UTF-8 src/MavenLite.java && echo -e "\nCompilation terminée avec succès\n" || echo -e "\nErreur lors de la compilation\n"
