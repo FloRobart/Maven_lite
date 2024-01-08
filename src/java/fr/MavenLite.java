98dc85254bc8b32f74e7f43baaa663a7a0331ad6
Floris <florisrobart.pro@gmail.com>
2024-01-08 16:43:01 +0100

    correction de bug

--- src/java/fr/MavenLite.java
+++ src/java/fr/MavenLite.java
@@ -895,7 +895,7 @@ public class MavenLite
             System.exit(1);
         }
 
-        /* Compilation */this.removeFile(MavenLite.COMPILE_LIST_NAME);
+        /* Compilation */
         command.append("javac -d ").append(this.hmArgs.get("target"));
 
         if (this.hmArgs.get("libraries") != null)
@@ -912,15 +912,15 @@ public class MavenLite
         if (this.hmArgs.get(this.lstOptions.get(8)[0]) != null)
             System.out.println(MavenLite.INFO + MavenLite.BLUE_BOLD + command.toString() + MavenLite.DEFAULT);
 
-        System.out.println(MavenLite.INFO + "Compilation du projet '" + MavenLite.BLUE_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "'...");
+        System.out.println(MavenLite.INFO + "Compilation du projet '" + MavenLite.BLUE_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "'...\n");
         if (this.executCommande(command.toString()) != 0)
         {
-            System.out.println(MavenLite.ERROR + "La compilation du projet '" + MavenLite.RED_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "' à échoué.");
+            System.out.println("\n" + MavenLite.ERROR + "La compilation du projet '" + MavenLite.RED_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "' à échoué.");
             this.removeFile(MavenLite.COMPILE_LIST_NAME);
             System.exit(1);
         }
         
-        System.out.println(MavenLite.SUCCESS + "Compilation du projet '" + MavenLite.GREEN_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "' terminé avec succès.");
+        System.out.println("\n" + MavenLite.SUCCESS + "Compilation du projet '" + MavenLite.GREEN_BOLD + MavenLite.PROJECT_NAME + MavenLite.DEFAULT + "' terminé avec succès.");
         this.removeFile(MavenLite.COMPILE_LIST_NAME);
     }
 
