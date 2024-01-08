af6d68aff51b3693b0e9c33d2d3285ff9ba6c641
Floris <florisrobart.pro@gmail.com>
2024-01-08 15:23:26 +0100

    correction des affichages

--- src/installation/en/install.sh
+++ src/installation/en/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Déplacement des fichiers shell dans le dossier /usr/local/bin
-sudo mv mvnl /usr/local/bin || { echo 'Error occurred while moving the \'mvnl\' file.'; exit 1; }
-sudo mv mvnl-uninstall /usr/local/bin/ || { echo 'Error occurred while moving the \'mvnl-uninstall\' file.'; exit 1; }
+sudo mv mvnl /usr/local/bin || { echo "Error occurred while moving the 'mvnl' file."; exit 1; }
+sudo mv mvnl-uninstall /usr/local/bin/ || { echo "Error occurred while moving the 'mvnl-uninstall' file."; exit 1; }
 
 # Déplacement du dossier maven-lite dans /etc
-sudo mv maven-lite /etc || { echo 'Error occurred while moving the \'maven-lite\' folder.'; exit 1; }
+sudo mv maven-lite /etc || { echo "Error occurred while moving the 'maven-lite' folder."; exit 1; }
 
 # Déplacement des pages de manuel dans /usr/local
-sudo mv man /usr/local/ || { echo 'Error occurred while moving the manual pages.'; exit 1; }
+sudo mv man /usr/local/ || { echo "Error occurred while moving the manual pages."; exit 1; }
 
 echo 'Installation completed successfully.'
 exit 0
