522707cbe2d21a50ad39b4e3b6ae2a59c0de32a4
Floris <florisrobart.pro@gmail.com>
2024-01-09 03:09:26 +0100

    création des package pour debian, linux, mac et windows (sans les installer)

--- src/batch/mvnl.bat
+++ src/batch/mvnl.bat
@@ -1,25 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "args="
+echo %*
 
-REM Parcourir tous les arguments en ligne de commande
-for %%i in (%*) do (
-    REM Si l'argument commence par un backslash, on le double
-    set "arg=%%i"
-    if "!arg:~0,1!"=="\" (
-        set "args=!args! ^"!arg!"^"
-    ) else (
-        set "args=!args! !arg!"
-    )
-)
-
-echo java -cp "C:\Users\%USER_NAME%\.maven-lite\" MavenLite %args%
-
-endlocal
-exit /b
-
-
-start java -cp "C:\Users\%USER_NAME%\.maven-lite\" MavenLite %args%
+start java -cp "C:\Users\%USER_NAME%\.maven-lite\" MavenLite %*
 
 endlocal
